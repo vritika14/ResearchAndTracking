ed8d0e7200ba7f650d6aa34289afe69696bfb542
Vritika14 <vritika.tuteja@students.mq.edu.au>
2026-07-28 12:26:40 +1000

    Added health/live endpoint

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(private readonly health: HealthCheckService) {}
+
+  @Get('live')
+  @HealthCheck()
+  checkLiveness() {
+    return this.health.check([]);
+  }
+}
--- apps/api/src/health/health.module.ts
+++ apps/api/src/health/health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  controllers: [HealthController]
+  imports: [TerminusModule],
+  controllers: [HealthController],
 })
 export class HealthModule {}
